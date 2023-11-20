065161f46d5f70d2fb00f7a4f8792240b2d1e6b0
Sungsu <127682098+sonsu95@users.noreply.github.com>
2023-11-21 00:49:47 +0900

    Time: 60 ms (64.57%), Space: 44.6 MB (88.14%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.ts
@@ -0,0 +1,13 @@
+function fizzBuzz(n: number): string[] {
+  const arr: string[] = [];
+  for (let i = 1; i <= n; i++) {
+    i % 15 === 0
+      ? arr.push("FizzBuzz")
+      : i % 3 === 0
+        ? arr.push("Fizz")
+        : i % 5 === 0
+          ? arr.push("Buzz")
+          : arr.push(i.toString());
+  }
+  return arr;
+}
