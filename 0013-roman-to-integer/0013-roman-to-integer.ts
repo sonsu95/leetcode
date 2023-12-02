d6dda3926f25914d2259e429c27dbc0b5aa4ac40
Sungsu <127682098+sonsu95@users.noreply.github.com>
2023-12-02 14:14:23 +0900

    Time: 114 ms (53.16%), Space: 48 MB (70.39%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.ts
@@ -0,0 +1,25 @@
+function romanToInt(s: string): number {
+  const symbol = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  }
+
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    const cur = symbol[s[i]];
+    const next = symbol[s[i + 1]];
+
+    if (cur < next) {
+      result += next - cur;
+      i++;
+    } else {
+      result += cur;
+    }
+  }
+  return result;
+};
